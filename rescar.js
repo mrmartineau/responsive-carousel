6e088099264b07feeb60aed5df4480b9827d99d0
Zander Martineau <zander@martineau.tv>
2013-08-15 01:03:10 +0100

    Get/Set CSS styles with ease

--- rescar.js
+++ rescar.js
@@ -70,6 +70,44 @@
 		return document.querySelector(el);
 	}
 
+	// Get/Set CSS styles with ease
+	function styler(el) {
+		return {
+			/* Get CSS style
+			 * @prop : String - CSS property name e.g 'width', 'height'
+			 * @int  : Boolean
+			 */
+			get : function(prop, int) {
+				/* TODO:
+				 * Get multiple CSS properties. prop could be comma separated list
+				 * Allow get all css values
+				 * Throw errors when args not in correct format
+				 */
+				var int = int || false;
+
+				if (int === true) {
+					return parseInt(window.getComputedStyle(el, null).getPropertyValue(prop), 10);
+				} else{
+					return window.getComputedStyle(el, null).getPropertyValue(prop);
+				}
+			},
+
+			/* Set CSS style
+			 * @prop : String - CSS property name e.g 'width', 'height'
+			 * @val  : String - CSS property value e.g. '200px'
+			 */
+			set : function(prop, val) {
+				/* TODO:
+				 * Set multiple CSS properties. prop could be comma separated list
+				 * Throw errors when args not in correct format
+				 */
+				return el.style[prop] = val;
+			}
+		};
+	};
+
+
+
 	// -------------------------- init -------------------------- //
 
 	// TODO:
